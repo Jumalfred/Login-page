dfe3cdf74d5539cc90ff38aec7c58d9162c99a93
Alfred Juma <jumextoginga@gmail.com>
2022-03-15 09:10:56 +0300

    login-page

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var greeting;
-    var time = new Date().getHours();
-    if (time < 12) {
-      greeting = "Good morning";
-    } else if (time < 16) {
-      greeting = "Good afternoon";
-    }
-    else {
-      greeting = "Good evening";
-    }
-    document.getElementById("dog").innerHTML = greeting;
-    function validateForm() {
-        var x = document.forms["myForm"]["password"].value;
-        var y = document.forms["myForm"]["pasword"].value;
-        if (x !=y) {
-          document.getElementById("demo").innerHTML="Password missmatch !,please try again"
-          return false;
-        }
-      }
-      
+var time = new Date().getHours();
+if (time => 0 && time < 12){
+  greeting = "Good morning";
+  } 
+else if (time => 12 && time < 16) {
+  greeting = "Good afternoon";
+
+}
+  greeting = "Good evening";
+}
+document.getElementById("dog").innerHTML = greeting;
+function validateForm() {
+  var x = document.forms["myForm"]["password"].value;
+  var y = document.forms["myForm"]["pasword"].value;
+  if (x != y) {
+    document.getElementById("demo").innerHTML = "password missmatch !,please try again";
+    return false;
+  }
+}
